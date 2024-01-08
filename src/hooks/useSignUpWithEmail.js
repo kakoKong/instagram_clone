f6438799e6b966afaa6577b1091360f1d19def1c
Kong <khaokong@gmail.com>
2024-01-08 22:03:15 +0000

    fix: env

---
+++ src/hooks/useSignUpWithEmail.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth, firestore } from '../firebase/firebase'
+import { setDoc, doc } from 'firebase/firestore';
+
+const useSignUpWithEmail = () => {
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
+
+  const signup = async (inputs) => {
+    if(!inputs.email || !inputs.password || !inputs.username || inputs.fullName) {
+      console.log("Please fill all the fields")
+      return
+    }
+    try{
+      const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password)
+      if (!newUser && error) {
+        console.log(error)
+        return
+      }
+      if(newUser) {
+        const userDoc = {
+          uid: newUser.user.uid,
+          email: inputs.email,
+          username: inputs.username,
+          fullName: inputs.fullName,
+          bio: "",
+          profilePicURL: "",
+          following: [],
+          followers: [],
+          posts: [],
+          createdAt: Date.now()
+        }
+
+        await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+        localStorage.setItem("user-info", JSON.stringify(userDoc))
+      }
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+  return {
+    loading, error, signup
+  }
+}
+
+export default useSignUpWithEmail
