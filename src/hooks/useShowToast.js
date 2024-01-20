41b87c0d84c1fa4a7f1c7732fdb9c8ac6f55d701
Kong <khaokong@gmail.com>
2024-01-20 21:08:57 +0000

    :art: Add userprofile store
    add userprofile store to be able to get the user details on userprofile page

--- src/hooks/useShowToast.js
+++ src/hooks/useShowToast.js
@@ -1,8 +1,9 @@
 import { useToast } from '@chakra-ui/react'
+import { useCallback } from 'react';
 
 const useShowToast = () => {
   const toast = useToast()
-  const showToast = (title, description, status ) => {
+  const showToast = useCallback((title, description, status ) => {
     toast({
       title: title,
       description: description,
@@ -10,7 +11,8 @@ const useShowToast = () => {
       duration: 3000,
       isCloasble: true
     });
-  };
+  }, [toast])
+  
 
   return showToast;
 }
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,14 +1,23 @@
-import { Container, Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Container, Flex, Link, Skeleton, SkeletonCircle, Text, VStack } from '@chakra-ui/react'
 import ProfileHeader from '../../components/Profile/ProfileHeader'
 import { ProfileTab } from '../../components/Profile/ProfileTab'
 import ProfilePosts from '../../components/Profile/ProfilePosts'
+import useGetUserProfileByUsername from '../../hooks/useGetUserProfileByUsername'
+import { useParams } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
 
 const ProfilePage = () => {
+  const { username } = useParams()
+  const { isLoading, userProfile } = useGetUserProfileByUsername(username)
+
+  if (!isLoading && !userProfile) {
+    <UserNotFound />
+  }
   return (
     <Container maxW={"container.lg"} py={5}>
       <Flex py={10} px={4} pl={{ base: 4, mdd: 10 }} w={"full"} mx={"auto"} flexDirection={"column"}>
-        <ProfileHeader />
+        {!isLoading && userProfile && <ProfileHeader />}
+        {isLoading && <ProfileHeaderSkeleton />}
       </Flex>
       <Flex
         px={{ base: 2, sm: 4 }}
@@ -26,3 +35,34 @@ const ProfilePage = () => {
 }
 
 export default ProfilePage
+
+const ProfileHeaderSkeleton = () => {
+  return(
+    <Flex
+      gap={{ base: 4, sm: 30}}
+      py={10}
+      direction={{ base: "column", sm: "row"}}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <SkeletonCircle size="24" />
+
+      <VStack alignItems={{base: 'center', sm: 'flex-start'}} gap={2} mx={'auto'} flex={1}>
+        <Skeleton height="12px" width="150px" />
+        <Skeleton height="12px" width="150px" />
+      </VStack>
+
+    </Flex>
+  )
+}
+
+const UserNotFound = () => {
+  return (
+    <Flex flexDir="column" textAlign={"center"} mx={"auto"}>
+      <Text fontSize={"2x1"}>User Not Found</Text>
+      <Link as={RouterLink} to={"/"} color={"blue.500"} w={"max-content"} mx={"auto"}>
+        Go Home
+      </Link>
+    </Flex>
+  )
+}
