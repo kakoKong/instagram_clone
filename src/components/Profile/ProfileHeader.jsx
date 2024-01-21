f770a8252d0dfd3f3b74147ce5017d63f264cf4d
Kong <khaokong@gmail.com>
2024-01-21 10:58:54 +0000

    :sparkles: Follow button
    create follow button on different user profile page

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,8 +1,12 @@
 import { Avatar, AvatarGroup, Flex, VStack, Text, Button } from '@chakra-ui/react'
 import useUserProfileStore from '../../store/userProfileStore';
+import useAuthStore from '../../store/authStore';
 
 const ProfileHeader = () => {
+  const authUser = useAuthStore(state => state.user)
   const userProfile = useUserProfileStore(state => state.userProfile)
+  const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
+  const visitingOtherProfileAndAuth = authUser && authUser.username !== userProfile.username;
   console.log(userProfile)
   return (
     <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: "column", sm: "row" }}>
@@ -10,20 +14,39 @@ const ProfileHeader = () => {
         <Avatar name={userProfile.username} src={userProfile.profulePicURL} />
       </AvatarGroup>
       <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
-        <Flex
-          gap={4}
-          direction={{ base: "column", sm: "row" }}
-          justifyContent={{ base: "center", sm: "flex-start" }}
-          alignItems={"center"}
-          w={"full"}
-        >
-          <Text fontSize={{ base: "sm", md: "lg" }}>{userProfile.username}</Text>
-          <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
-            <Button bg={"white"} color={"black"} _hover={{ bg: "whiteAlpha.700" }} size={{ base: "xs", md: "sm" }}>
-              Edit Profile
-            </Button>
+        {visitingOwnProfileAndAuth && (
+          <Flex
+            gap={4}
+            direction={{ base: "column", sm: "row" }}
+            justifyContent={{ base: "center", sm: "flex-start" }}
+            alignItems={"center"}
+            w={"full"}
+          >
+            <Text fontSize={{ base: "sm", md: "lg" }}>{userProfile.username}</Text>
+            <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+              <Button bg={"white"} color={"black"} _hover={{ bg: "whiteAlpha.700" }} size={{ base: "xs", md: "sm" }}>
+                Edit Profile
+              </Button>
+            </Flex>
           </Flex>
-        </Flex>
+        )}
+
+        {visitingOtherProfileAndAuth && (
+          <Flex
+            gap={4}
+            direction={{ base: "column", sm: "row" }}
+            justifyContent={{ base: "center", sm: "flex-start" }}
+            alignItems={"center"}
+            w={"full"}
+          >
+            <Text fontSize={{ base: "sm", md: "lg" }}>{userProfile.username}</Text>
+            <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+              <Button bg={"blue.500"} color={"white"} _hover={{ bg: "blue.600" }} size={{ base: "xs", md: "sm" }}>
+                Follow
+              </Button>
+            </Flex>
+          </Flex>
+        )}
         <Flex alignItems={"center"} gap={{ base: 2, sm: 4 }}>
           <Text fontSize={{ base: "xs", md: "sm" }}>
             <Text as="span" fontWeight={"bold"} mr={1}>
@@ -33,13 +56,13 @@ const ProfileHeader = () => {
           </Text>
           <Text fontSize={{ base: "xs", md: "sm" }}>
             <Text as="span" fontWeight={"bold"} mr={1}>
-            {userProfile.followers.length}
+              {userProfile.followers.length}
             </Text>
             Followers
           </Text>
           <Text fontSize={{ base: "xs", md: "sm" }}>
             <Text as="span" fontWeight={"bold"} mr={1}>
-            {userProfile.following.length}
+              {userProfile.following.length}
             </Text>
             Following
           </Text>
